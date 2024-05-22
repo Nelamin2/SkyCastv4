d13dd8b1920f3f6b754d6c7ef90a51e10723c7be
Nidal <140646982+Nelamin2@users.noreply.github.com>
2024-05-22 11:18:52 +0300

    Add files via upload
    uploading app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def fetch_weather(api_key, location, country_code="us"):
+    # Check if the location is a zip code (assuming it only contains digits)
+    if location.isdigit():
+        api_url = "http://api.openweathermap.org/data/2.5/weather?zip={},{}&units=imperial&appid={}".format(location, country_code, api_key)
+    else:
+        api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}".format(location, api_key)
+    
+    r = requests.get(api_url)
+    return r.json()
+
+# Example usage:
+api_key = '3450acc3ccb969baf99ddbcb6796e04e'
+print(fetch_weather(api_key, 'San Francisco'))  # Fetch by city name
+print(fetch_weather(api_key, '94103'))  # Fetch by zip code
+
+if __name__ == '__main__':
+    app.run(debug=True)
